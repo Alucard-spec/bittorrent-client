315fe5f8fedeaee14cc7d6028d1a751e46ef9e9d
Ujjawal Verma <ujjawal.verma9651@gmail.com>
2024-07-16 11:57:28 +0530

    add handshake handler and onWholeMessage

--- src/download.js
+++ src/download.js
@@ -2,21 +2,53 @@
 
 import net from "net";
 import {Buffer} from "buffer";
-import {getPeers} from "./tracker.js"
+import {getPeers} from "./tracker.js";
+
+import {buildBitField,buildCancel,buildChoke,buildHandshake,buildHave,buildInterested,buildKeepAlive,buildPiece,buildPort,buildRequest,buildUnchoke,buildUninterested} from "./message.js";
+
 export function downloads(torrent){
     getPeers(torrent,peers=>{
-        peers.forEach(download);
+        peers.forEach(peer => download(peer,torrent));
     });
 };
-function download(peer){
+function download(peer,torrent){
     const socket = net.Socket();
     socket.on('error',console.log("Error recieved"));
     socket.connect(peer.port, peer.ip,()=>{
-        //Write the message here
+        // 1
+        socket.write(buildHandshake(torrent));
     });
-    socket.on('data',data=>{
-        //handle the response here
-    });
+    onWholeMsg(socket, msg => {
+        msgHandler(msg,socket);   
+      });    
 }
 
+
+
 // so what we are doing in the above functions is get the peers from the get peer function of tracker.js and then creatig a tcp connection with each of those peers and start exchanging messages.
+
+function msgHandler(msg, socket) {
+    if (isHandshake(msg)) socket.write(message.buildInterested());
+  }
+
+  function isHandshake(msg) {
+    return msg.length === msg.readUInt8(0) + 49 &&
+           msg.toString('utf8', 1) === 'BitTorrent protocol';
+  }
+
+function onWholeMsg(socket, callback){
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+  
+    socket.on('data', recvBuf => {
+      // msgLen calculates the length of a whole message
+      const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+      savedBuf = Buffer.concat([savedBuf, recvBuf]);
+  
+      while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+        callback(savedBuf.subarray(0, msgLen()));
+        savedBuf = savedBuf.subarray(msgLen());
+        handshake = false;
+      }
+    });
+}
